4171b239ddbcb952996fe95c063c0b66d4c1a734
Nicolas Raoul <nicolas.raoul@gmail.com>
2012-11-26 19:43:50 +0900

    Automatically use latest data available

--- compare.sh
+++ compare.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-DATE="20121125"
+# Find the latest available date
+LATEST_DATE=`ls wv* | sort -r | head -n 1 | sed -e "s/wv//"`
+
+# Use date provided on command line, or latest if none provided
+DATE=${1:-$LATEST_DATE}
 echo "Stats for $DATE"
 
 echo "Number of entries in recent changes edits (including deletions/blocks):"
